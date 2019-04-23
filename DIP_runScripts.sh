7834f9da2732de1b24f2e097950a52da20a1c671
LambdaDevBox <jessicaloohw@gmail.com>
2019-04-23 15:33:19 -0400

    add website stuff

--- DIP_runScripts.sh
+++ DIP_runScripts.sh
@@ -1,28 +1,43 @@
 #!/bin/bash
+
+python training.py '1' 'unet' 'mse' 'adam' 0.0001 50000 1000
+python training.py '2_R' 'unet' 'mse' 'adam' 0.0001 50000 1000
+python training.py '3' 'unet' 'mse' 'adam' 0.0001 50000 1000
+python training.py '4' 'unet' 'mse' 'adam' 0.0001 50000 1000
+python training.py '5' 'unet' 'mse' 'adam' 0.0001 50000 1000
+
+python training.py '1' 'unet' 'l1' 'adam' 0.0001 50000 1000
+python training.py '2_R' 'unet' 'l1' 'adam' 0.0001 50000 1000
+python training.py '3' 'unet' 'l1' 'adam' 0.0001 50000 1000
+python training.py '4' 'unet' 'l1' 'adam' 0.0001 50000 1000
+python training.py '5' 'unet' 'l1' 'adam' 0.0001 50000 1000
+
 python training.py '1' 'unet' 'mse_l1' 'adam' 0.0001 50000 1000
 python training.py '2_R' 'unet' 'mse_l1' 'adam' 0.0001 50000 1000
 python training.py '3' 'unet' 'mse_l1' 'adam' 0.0001 50000 1000
 python training.py '4' 'unet' 'mse_l1' 'adam' 0.0001 50000 1000
 python training.py '5' 'unet' 'mse_l1' 'adam' 0.0001 50000 1000
 
-#python training.py '2_R'
-#python training.py '3'
-#python training.py '4'
-#python training.py '5'
-#python training.py '6'
-#python training.py '7'
-#python training.py '8'
-#python training.py '9'
-#python training.py '10'
-#python training.py '11'
-#python training.py '12'
-#python training.py '13'
-#python training.py '14'
-#python training.py '15'
-#python training.py '16'
-#python training.py '17'
-#python training.py 'D_1'
-#python training.py 'D_2'
-#python training.py 'D_3'
-#python training.py 'V22'
-#python training.py 'V_1'
+python training.py '1' 'unet' 'mse_with_tv_reg' 'adam' 0.0001 50000 1000 1e-6 0.0 10.0
+python training.py '2_R' 'unet' 'mse_with_tv_reg' 'adam' 0.0001 50000 1000 1e-6 0.0 10.0
+python training.py '3' 'unet' 'mse_with_tv_reg' 'adam' 0.0001 50000 1000 1e-6 0.0 10.0
+python training.py '4' 'unet' 'mse_with_tv_reg' 'adam' 0.0001 50000 1000 1e-6 0.0 10.0
+python training.py '5' 'unet' 'mse_with_tv_reg' 'adam' 0.0001 50000 1000 1e-6 0.0 10.0
+
+python training.py '1' 'unet' 'mse_with_edge_reg' 'adam' 0.0001 50000 1000 1e-6 1e-6 10.0
+python training.py '2_R' 'unet' 'mse_with_edge_reg' 'adam' 0.0001 50000 1000 1e-6 1e-6 10.0
+python training.py '3' 'unet' 'mse_with_edge_reg' 'adam' 0.0001 50000 1000 1e-6 1e-6 10.0
+python training.py '4' 'unet' 'mse_with_edge_reg' 'adam' 0.0001 50000 1000 1e-6 1e-6 10.0
+python training.py '5' 'unet' 'mse_with_edge_reg' 'adam' 0.0001 50000 1000 1e-6 1e-6 10.0
+
+python training.py '1' 'deep_decoder' 'mse_with_tv_reg' 'adam' 0.0001 50000 1000 1e-6 0.0 10.0
+python training.py '2_R' 'deep_decoder' 'mse_with_tv_reg' 'adam' 0.0001 50000 1000 1e-6 0.0 10.0
+python training.py '3' 'deep_decoder' 'mse_with_tv_reg' 'adam' 0.0001 50000 1000 1e-6 0.0 10.0
+python training.py '4' 'deep_decoder' 'mse_with_tv_reg' 'adam' 0.0001 50000 1000 1e-6 0.0 10.0
+python training.py '5' 'deep_decoder' 'mse_with_tv_reg' 'adam' 0.0001 50000 1000 1e-6 0.0 10.0
+
+python training.py '1' 'deep_decoder' 'mse_with_edge_reg' 'adam' 0.0001 50000 1000 1e-6 1e-6 10.0
+python training.py '2_R' 'deep_decoder' 'mse_with_edge_reg' 'adam' 0.0001 50000 1000 1e-6 1e-6 10.0
+python training.py '3' 'deep_decoder' 'mse_with_edge_reg' 'adam' 0.0001 50000 1000 1e-6 1e-6 10.0
+python training.py '4' 'deep_decoder' 'mse_with_edge_reg' 'adam' 0.0001 50000 1000 1e-6 1e-6 10.0
+python training.py '5' 'deep_decoder' 'mse_with_edge_reg' 'adam' 0.0001 50000 1000 1e-6 1e-6 10.0
